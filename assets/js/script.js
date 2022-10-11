f7dd91cc1517f02c05860518b68f662e49485090
dnsgk4983 <dnsgk4983@gmail.com>
2022-10-11 23:39:34 +0900

    1011

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,11 @@
-
+const mask1 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".section-intro",
+    start: "top top",
+    end: "50% 50%",
+    scrub: true,
+  }
+});
 const mask2 = gsap.timeline({
   scrollTrigger: {
     trigger: ".section-intro",
@@ -7,7 +14,40 @@ const mask2 = gsap.timeline({
     scrub: true,
   },
 });
+mask1.to(".section-intro .intro-mask-2", {
+  opacity: 1,
+  duration: 1
+});
 mask2.to(".section-intro .mask-2", {
   opacity: 1,
   duration: 1
 });
+
+
+let menuBtn = document.querySelector(".mobile-menu");
+let nav = document.querySelector(".nav-content");
+menuBtn.addEventListener("click", (e) => {
+  nav.classList.toggle("on");
+});
+
+var galleryTop = new Swiper('.team-gallery', {
+  spaceBetween: 10,
+  effect: "fade",
+  loop: true,
+  loopedSlides: 3
+});
+var galleryThumbs = new Swiper('.gallery-thumbs', {
+  spaceBetween: 10,
+  direction: "vertical",
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  touchRatio: 0.2,
+  slideToClickedSlide: true,
+  loop: true,
+  loopedSlides: 3
+});
+galleryTop.controller.control = galleryThumbs;
+galleryThumbs.controller.control = galleryTop;
