80ad86fadb3f9f3a0f0fe77753b8ab0882b0654d
zensuyeom <zensuyeom@gmail.com>
2022-10-12 14:44:43 +0900

    multi 섹션 스크립트 작업
    multi 섹션 스크립트 작업

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ const mask1 = gsap.timeline({
     start: "top top",
     end: "50% 50%",
     scrub: true,
-  }
+  },
 });
 const mask2 = gsap.timeline({
   scrollTrigger: {
@@ -16,13 +16,12 @@ const mask2 = gsap.timeline({
 });
 mask1.to(".section-intro .intro-mask-2", {
   opacity: 1,
-  duration: 1
+  duration: 1,
 });
 mask2.to(".section-intro .mask-2", {
   opacity: 1,
-  duration: 1
+  duration: 1,
 });
-
 
 let menuBtn = document.querySelector(".mobile-menu");
 let nav = document.querySelector(".nav-content");
@@ -30,24 +29,41 @@ menuBtn.addEventListener("click", (e) => {
   nav.classList.toggle("on");
 });
 
-var galleryTop = new Swiper('.team-gallery', {
+var galleryTop = new Swiper(".team-gallery", {
   spaceBetween: 10,
   effect: "fade",
   loop: true,
-  loopedSlides: 3
+  loopedSlides: 3,
 });
-var galleryThumbs = new Swiper('.gallery-thumbs', {
+var galleryThumbs = new Swiper(".gallery-thumbs", {
   spaceBetween: 10,
   direction: "vertical",
   pagination: {
     el: ".swiper-pagination",
   },
   centeredSlides: true,
-  slidesPerView: 'auto',
+  slidesPerView: "auto",
   touchRatio: 0.2,
   slideToClickedSlide: true,
   loop: true,
-  loopedSlides: 3
+  loopedSlides: 3,
 });
 galleryTop.controller.control = galleryThumbs;
 galleryThumbs.controller.control = galleryTop;
+
+// 221012 jinsu
+const multiChainSection = document.querySelector(".multi-chain_wrap");
+let scrollTop;
+let offsetTopMulti;
+let multiHeight;
+let scrollRealHeight;
+let percent;
+
+window.addEventListener("scroll", () => {
+  scrollTop = window.pageYOffset;
+  offsetTopMulti = multiChainSection.getBoundingClientRect().top + scrollTop;
+  multiHeight = multiChainSection.offsetHeight;
+  scrollRealHeight = multiHeight - window.innerHeight;
+  percent = ((scrollTop - offsetTopMulti) / scrollRealHeight) * 100;
+  console.log(percent);
+});
