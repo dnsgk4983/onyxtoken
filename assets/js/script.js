a71c8b5d1737d9ca9ab6085e9146124540bd2116
유경상 <dbrudtkd93@naver.com>
2022-10-19 22:41:20 +0900

    수정사항 추가

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
+// 드롭다운
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+if (!event.target.matches('.dropbtn')) {
+
+  var dropdowns = document.getElementsByClassName("dropdown-content");
+  var i;
+  for (i = 0; i < dropdowns.length; i++) {
+    var openDropdown = dropdowns[i];
+    if (openDropdown.classList.contains('show')) {
+      openDropdown.classList.remove('show');
+    }
+  }
+}
+}
+
 const mask1 = gsap.timeline({
   scrollTrigger: {
     trigger: ".section-intro",
