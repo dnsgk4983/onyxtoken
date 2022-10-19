54c2d85c506a1d4e489320e1d820b56de4ca65b6
hwayungyi90 <hwayoung0716@gmail.com>
2022-10-19 23:43:18 +0900

    드롭다운메뉴 반응형 작업

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ function myFunction() {
 window.onclick = function(event) {
 if (!event.target.matches('.dropbtn')) {
 
-  var dropdowns = document.getElementsByClassName("dropdown-content");
+  var dropdowns = document.querySelector("community");
   var i;
   for (i = 0; i < dropdowns.length; i++) {
     var openDropdown = dropdowns[i];
